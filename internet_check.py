de0fd1e7a57d075d7100f0c7f89acc5e0c2c9310
KaiserMovet <rynkiewiczmate@gmail.com>
2020-05-29 14:03:05 +0200

    added speed_status

--- internet_check.py
+++ internet_check.py
@@ -1,21 +1,31 @@
 from datetime import datetime
 import requests
 import time
-
+from speed_status import SpeedStatus
 from connection_status import ConnectionStatus
 
 TARGET_ADDRESS = "http://google.com"
-INTERVAL_STEP = 5
+INTERVAL_STEP = 300
+SPEED_CHECK_STEP = 12
 LOG_FILE = 'log.txt'
+SPEED_LOG_FILE = 'speed_log.txt'
 
 
 def main():
 
     connection_status = ConnectionStatus(TARGET_ADDRESS, LOG_FILE)
-
+    speed_status = SpeedStatus(SPEED_LOG_FILE)
+    index = -1
     while True:
+        index = (index + 1) % SPEED_CHECK_STEP
+        print(index)
         time.sleep(INTERVAL_STEP)
-        connection_status.loop()
+        has_connection = connection_status.loop()
+        if index == 0:
+            if has_connection:
+                speed_status.speed_status()
+            else:
+                speed_status.push_empty_entry()
 
 
 if __name__ == "__main__":
--- speed_status.py
+++ speed_status.py
@@ -1,6 +1,60 @@
-import requests
 from datetime import datetime
+import speedtest
 
 
 class SpeedStatus():
+
+    TIME_STR = "%Y.%m.%d - %H:%M:%S"
+
+    def __init__(self, log_file):
+        self.log_file = log_file
+        pass
+
+    def push_empty_entry(self):
+        results_dict = {"download": 0, "upload": 0}
+        entry = self._generate_entry(results_dict)
+        self._save_info(entry)
+        pass
+
+    def speed_status(self):
+        results_dict = self._speed_test()
+        entry = self._generate_entry(results_dict)
+        self._save_info(entry)
+        pass
+
+    def _save_info(self, text):
+        with open(self.log_file, 'a') as file:
+            file.writelines([F'{text}\n'])
+
+    @classmethod
+    def _generate_entry(cls, results):
+        time_log = datetime.now().strftime(cls.TIME_STR)
+        # download speed in megabits
+        download = round(results["download"] / 1000000, 1)
+        # upload speed in megabits
+        upload = round(results["upload"] / 1000000, 1)
+
+        return F"[{time_log}] {download} {upload}"
+
+    @staticmethod
+    def _speed_test():
+
+        servers = []
+        threads = None
+
+        s = speedtest.Speedtest()
+        s.get_servers(servers)
+        s.get_best_server()
+        s.download(threads=threads)
+        s.upload(threads=threads)
+        s.results.share()
+
+        return s.results.dict()
+
+
+def main():
     pass
+
+
+if __name__ == "__main__":
+    main()
